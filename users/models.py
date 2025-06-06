dd5391f115ad6fe72dd0629646be55fb2b53f14c
Farhod <farhod@example.com>
2025-06-07 01:34:32 +0500

    4

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -11,13 +12,23 @@ class UserManager(BaseUserManager):
         if not full_name:
             raise ValueError('The Full Name field must be set')
 
+        extra_fields.setdefault('is_active', True)
+
         user = self.model(phone=phone, full_name=full_name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, phone, full_name, password=None, **extra_fields):
-        # Bu yerda is_staff, is_superuser, is_active kerak emas
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
         return self.create_user(phone, full_name, password, **extra_fields)
 
 
@@ -27,13 +38,7 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
         ('Buyer', 'Xaridor'),
     ]
 
-    # GENDER_CHOICES = [
-    #     ('Male', 'Erkak'),
-    #     ('Female', 'Ayol'),
-    # ]
-
-    role = models.CharField(max_length=50, choices=ROLE_CHOICES, )
-    # gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='Male')
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
     full_name = models.CharField(max_length=255)
     phone = models.CharField(max_length=15, unique=True)
     passport_scan = models.ImageField(upload_to='passport_scan/', blank=True, null=True)
@@ -42,10 +47,14 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
     passport_seria = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    #  Django admin ishlashi uchun majburiy maydonlar:
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
     objects = UserManager()
 
-    USERNAME_FIELD = 'phone'  # login uchun foydalaniladigan field
+    USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = ['full_name']
 
-def __str__(self):
+    def __str__(self):
         return f"{self.full_name} | {self.phone}"
